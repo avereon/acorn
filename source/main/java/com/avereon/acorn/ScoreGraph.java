5506cdf028fb538ee9b0a52f94c34657e5cd2401
Mark Soderquist <mark@soderquist.net>
2022-06-12 15:31:00 -0600

    Add score graph

--- source/main/java/com/avereon/acorn/ScoreGraph.java
+++ source/main/java/com/avereon/acorn/ScoreGraph.java
@@ -50,6 +50,7 @@ public class ScoreGraph extends Pane {
 		getChildren().addAll( divider );
 
 		addScore( new AcornScore( false, 270, "16-Core AMD Ryzen 9 5950X One Thread" ) );
+		addScore( new AcornScore( false, 107, "4-Core Intel Core i3-2120 One Thread" ) );
 		addScore( new AcornScore( false, 20, "4-Core Raspberry PI 3 All Threads" ) );
 		addScore( new AcornScore( false, 6, "4-Core Raspberry PI 3 One Thread" ) );
 	}
